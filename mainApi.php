74598c427c9c8e6454d9006f85b168db4ea1a05d
Icaro Rocha <icaro.rocha@epitech.eu>
2020-03-07 12:34:33 +0100

    artist function changed

--- mainApi.php
+++ mainApi.php
@@ -4,7 +4,9 @@ require "connection.class.php";
 
 function searchArtist($artistQuery)
 {
-    $artistArray = Connection::getConnection()->prepare("SELECT * FROM `artists`
+    $artistArray = Connection::getConnection()->prepare("SELECT artists.id, artists.name, artists.description, 
+    artists.bio, artists.photo, (SELECT GROUP_CONCAT(name) FROM albums WHERE artist_id=artists.id ORDER BY release_date)
+    AS 'artist_albums' FROM `artists`
     WHERE name 
     LIKE '%" . $artistQuery . "%';");
     $artistArray->execute();
@@ -21,10 +23,11 @@ function searchAlbum($albumQuery)
 {
     $albumArray = Connection::getConnection()->prepare("SELECT albums.id, albums.name, albums.artist_id, 
     artists.name AS 'artist_name', albums.cover, albums.cover_small, albums.release_date, albums.popularity, 
-    albums.description, (SELECT group_concat(name) FROM tracks where album_id=albums.id ORDER BY track_no) AS 'track_list' 
+    albums.description, (SELECT GROUP_CONCAT(name) FROM tracks WHERE album_id=albums.id ORDER BY track_no) 
+    AS 'track_list' 
     FROM albums
     LEFT JOIN artists
-    ON albums.artist_id = artists.id WHERE albums.name LIKE '%". $albumQuery . "%'");
+    ON albums.artist_id = artists.id WHERE albums.name LIKE '%" . $albumQuery . "%'");
     $albumArray->execute();
     if ($albumArray->rowCount() < 1) {
         echo "Your search returned no queries." . PHP_EOL;
@@ -35,4 +38,47 @@ function searchAlbum($albumQuery)
     echo "Albums =>" . PHP_EOL . $albumJson;
 }
 
-searchAlbum('cre');
+function searchGenre($genreQuery)
+{
+    $albumArray = Connection::getConnection()->prepare("SELECT albums.id, albums.name, albums.artist_id, 
+    artists.name AS 'artist_name', albums.cover, albums.cover_small, albums.release_date, albums.popularity, 
+    albums.description, (SELECT GROUP_CONCAT(name) FROM tracks WHERE album_id=albums.id ORDER BY track_no) 
+    AS 'track_list' 
+    FROM albums
+    LEFT JOIN artists
+    ON albums.artist_id = artists.id WHERE albums.name LIKE '%" . $albumQuery . "%'");
+    $albumArray->execute();
+    if ($albumArray->rowCount() < 1) {
+        echo "Your search returned no queries." . PHP_EOL;
+        die;
+    }
+    $albumArray = $albumArray->fetchAll(PDO::FETCH_ASSOC);
+    $albumJson = json_encode($albumArray, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    echo "Albums =>" . PHP_EOL . $albumJson;
+}
+
+function searchAll($needle, $haystack)
+{
+    switch ($haystack) {
+        case "track": {
+                //implement code here
+                break;
+            }
+        case "album": {
+                searchAlbum($needle);
+                break;
+            }
+        case "artist": {
+                searchArtist($needle);
+                break;
+            }
+        case "genre": {
+                // implement code here
+                break;
+            }
+        default:
+            "Invalid command! Please try again.";
+    }
+}
+
+searchArtist('adam');
